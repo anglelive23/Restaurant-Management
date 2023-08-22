c54764e18e7188ead3e7c3c05dcd7a678b3cf813
anglelive23 <anglelive23@gmail.com>
2023-08-22 15:11:18 +0300

    APPLICATION: Add application logic in the request handler for Recipe
    Closes #6

---
+++ src/Core/RestaurantManagement.Application/Features/Recipes/GetRecipesListQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using RestaurantManagement.Application.Abstractions;
+using RestaurantManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Application.Features.Recipes
+{
+    public class GetRecipesListQueryHandler : IRequestHandler<GetRecipesListQuery, IQueryable<Recipe>>
+    {
+        #region Fields and Properties
+        private readonly IRecipeRepository _repo;
+        #endregion
+
+        #region Constructors
+        public GetRecipesListQueryHandler(IRecipeRepository repo)
+        {
+            _repo = repo;
+        }
+        #endregion
+
+        #region Interface Implementation
+        public async Task<IQueryable<Recipe>> Handle(GetRecipesListQuery request, CancellationToken cancellationToken)
+        {
+            var recipes = _repo.GetAllAsync();
+            return await Task.FromResult(recipes);
+        }
+        #endregion
+    }
+}
