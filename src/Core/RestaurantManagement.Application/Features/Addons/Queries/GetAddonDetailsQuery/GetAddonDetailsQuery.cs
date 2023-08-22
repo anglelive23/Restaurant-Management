e93c61563068529719830402dd6d705a52e78702
anglelive23 <anglelive23@gmail.com>
2023-08-22 17:40:03 +0300

    APPLICATION: Implement CQRS on Recipes
    Closes #8

---
+++ src/Core/RestaurantManagement.Application/Features/Addons/Queries/GetAddonDetailsQuery/GetAddonDetailsQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using RestaurantManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Application.Features.Addons.Queries.GetAddonDetailsQuery
+{
+    public class GetAddonDetailsQuery : IRequest<Addon?>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/Core/RestaurantManagement.Application/Features/Addons/Queries/GetAddonDetailsQuery/GetAddonDetailsQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using RestaurantManagement.Application.Abstractions;
+using RestaurantManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Application.Features.Addons.Queries.GetAddonDetailsQuery
+{
+    public class GetAddonDetailsQueryHandler : IRequestHandler<GetAddonDetailsQuery, Addon?>
+    {
+        #region Fields and Properties
+        private readonly IAddonsRepository _repo;
+        #endregion
+
+        #region Constructors
+        public GetAddonDetailsQueryHandler(IAddonsRepository repo)
+        {
+            _repo = repo ?? throw new ArgumentNullException(nameof(repo));
+        }
+        #endregion
+
+        #region Interface Implementation
+        public async Task<Addon?> Handle(GetAddonDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var addon = await _repo.GetByIdAsync(request.Id);
+            return addon;
+        }
+        #endregion
+    }
+}
