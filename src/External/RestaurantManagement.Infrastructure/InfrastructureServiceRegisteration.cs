bc66b167384c006cc81dee3baf384d4c7fd723e3
anglelive23 <anglelive23@gmail.com>
2023-08-24 13:20:38 +0300

    APPLICATION: Add support for JWT Authentication
    Closes #43

--- src/External/RestaurantManagement.Infrastructure/InfrastructureServiceRegisteration.cs
+++ src/External/RestaurantManagement.Infrastructure/InfrastructureServiceRegisteration.cs
@@ -5,6 +5,7 @@
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
             #region DbContext
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<RestaurantContext>();
             services.AddDbContext<RestaurantContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
             #endregion
@@ -14,6 +15,32 @@
             services.AddScoped<IAddonsRepository, AddonsRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IRecipeRepository, RecipeRepository>();
+            #endregion
+
+            #region JWT
+            services.Configure<JWT>(configuration.GetSection(nameof(JWT)));
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(opt =>
+            {
+                opt.RequireHttpsMetadata = false;
+                opt.SaveToken = false;
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidIssuer = configuration["JWT:Issuer"],
+                    ValidAudience = configuration["JWT:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"] ?? "OdkSeYWNl/ECZJaRsjzTqQ9rGb7jp0Ovp57idk1LeGs=")),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
             #endregion
 
             return services;
